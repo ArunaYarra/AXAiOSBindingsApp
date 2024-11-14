f4e11c74a9a3d4960e96a35a851c7c27bbcde814
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2024-11-14 16:52:06 +0530

    Sample App

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AXAiOSBindingsApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AXAiOSBindingsApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
