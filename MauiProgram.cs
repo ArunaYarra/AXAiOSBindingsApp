9b44543c8075f764da676a02aa265314bfb5bdbe
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2024-11-21 12:27:03 +0530

    SDK initialisation

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -9,7 +9,40 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .ConfigureLifecycleEvents(events =>
+            {
+#if IOS
+                events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) =>
+                {
+                    LogEvent(nameof(iOSLifecycle.FinishedLaunching));
+                    AXAiOSBindings_MAUI.CAMDOReporter.InitializeSDKWithOptions(AXAiOSBindings_MAUI.SDKOptions.SDKLogLevelVerbose, (_, __) => {
+                    {
+                        LogEvent("SDK initialised successfully");
+                    });
+                   return true;
+                }));
+
+                //Microsoft.Maui.Handlers.WebViewHandler.PlatformViewFactory = (handler) =>
+                //{
+                //    WKWebViewConfiguration config = MauiWKWebView.CreateConfiguration();
+                //    config.ApplicationNameForUserAgent = "MAAKitchenSink/1.0.0";
+                //    return new MauiWKWebView(CGRect.Empty, (WebViewHandler)handler, config);
+                //};
+#else
+                events.AddAndroid(android => android.OnRestart((activity) =>
+                {
+                    LogEvent(nameof(AndroidLifecycle.OnRestart));
+                    System.Diagnostics.Debug.WriteLine("Registered", "Registered for App Feedback ");
+                    //CaMDOIntegration.RegisterAppFeedBack(new Feedback());
+                }));
+#endif
+                static bool LogEvent(string eventName, string type = null)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Lifecycle event: {eventName}{(type == null ? string.Empty : $" ({type})")}");
+                    return true;
+                }
+            })
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
