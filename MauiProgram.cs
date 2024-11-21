fcb52e44b5509693a1296939d1014935af194d1d
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2024-11-21 12:48:51 +0530

    SDK initialisation

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace AXAiOSBindingsApp;
 
@@ -9,40 +11,34 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-            .ConfigureLifecycleEvents(events =>
-            {
-#if IOS
-                events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) =>
-                {
-                    LogEvent(nameof(iOSLifecycle.FinishedLaunching));
-                    AXAiOSBindings_MAUI.CAMDOReporter.InitializeSDKWithOptions(AXAiOSBindings_MAUI.SDKOptions.SDKLogLevelVerbose, (_, __) => {
-                    {
-                        LogEvent("SDK initialised successfully");
-                    });
-                   return true;
-                }));
+			// .ConfigureLifecycleEvents(events => {
+			// 	// #if IOS || MACCATALYST
+            //     //     events.AddiOS(ios => ios
+            //     //         .OnActivated((app) => LogEvent(nameof(iOSLifecycle.OnActivated)))
+            //     //         .OnResignActivation((app) => LogEvent(nameof(iOSLifecycle.OnResignActivation)))
+            //     //         .DidEnterBackground((app) => LogEvent(nameof(iOSLifecycle.DidEnterBackground)))
+            //     //         .WillTerminate((app) => LogEvent(nameof(iOSLifecycle.WillTerminate)))
+			// 	// .FinishedLaunching((app, launchOptions) =>
+            //     // {
+            //     //     LogEvent(nameof(iOSLifecycle.FinishedLaunching));
+			// 	// 	// AXAiOSBindings_MAUI.CAMDOReporter.InitializeSDKWithOptions(AXAiOSBindings_MAUI.SDKOptions.SDKLogLevelVerbose, (p1, p2) => {
 
-                //Microsoft.Maui.Handlers.WebViewHandler.PlatformViewFactory = (handler) =>
-                //{
-                //    WKWebViewConfiguration config = MauiWKWebView.CreateConfiguration();
-                //    config.ApplicationNameForUserAgent = "MAAKitchenSink/1.0.0";
-                //    return new MauiWKWebView(CGRect.Empty, (WebViewHandler)handler, config);
-                //};
-#else
-                events.AddAndroid(android => android.OnRestart((activity) =>
-                {
-                    LogEvent(nameof(AndroidLifecycle.OnRestart));
-                    System.Diagnostics.Debug.WriteLine("Registered", "Registered for App Feedback ");
-                    //CaMDOIntegration.RegisterAppFeedBack(new Feedback());
-                }));
-#endif
-                static bool LogEvent(string eventName, string type = null)
-                {
-                    System.Diagnostics.Debug.WriteLine($"Lifecycle event: {eventName}{(type == null ? string.Empty : $" ({type})")}");
-                    return true;
-                }
-            })
-            .ConfigureFonts(fonts =>
+			// 	// 	// });
+			// 	// 	// AXAiOSBindings_MAUI.CAMDOReporter.InitializeSDKWithOptions(AXAiOSBindings_MAUI.SDKOptions.SDKLogLevelVerbose, (_, __) => {
+   			// 	// 	// 	LogEvent("SDK initialized successfully");  
+   			// 	// 	// }); 
+			// 	// 	LogEvent("Initialisation done");
+			// 	// 	// AXAiOSBindings_MAUI.CAMDOReporter.UploadEventsWithCompletionHandler((_, __) => {});
+   			// 	// 	return true; 
+            //     // }));
+			// 	// #endif
+			// 	static bool LogEvent(string eventName, string type = null)
+			// 	{
+            //         System.Diagnostics.Debug.WriteLine($"Lifecycle event: {eventName}{(type == null ? string.Empty : $" ({type})")}");
+            //         return true;
+            //     }
+			// })
+			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace AXAiOSBindingsApp;
 
@@ -6,4 +7,12 @@ namespace AXAiOSBindingsApp;
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+		AXAiOSBindings_MAUI.CAMDOReporter.InitializeSDKWithOptions(AXAiOSBindings_MAUI.SDKOptions.SDKLogLevelVerbose, (_, __) => {
+   			System.Console.WriteLine("SDK initialized successfully");  
+   		});
+        return base.FinishedLaunching(application, launchOptions);
+    }
 }
